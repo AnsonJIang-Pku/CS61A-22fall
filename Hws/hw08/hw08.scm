bd053f95778f4dd36d317df9f2e9ef842aed9510
Zehui Jiang <75867171+AnsonJIang-Pku@users.noreply.github.com>
2023-08-29 00:40:57 +0800

    Add files via upload

--- Hws/hw08/hw08.scm
+++ Hws/hw08/hw08.scm
@@ -32,6 +32,30 @@
 		)
 )
 
+;;; 判断x是否在lst中
+(define (helper lst x)
+	(if (null? lst)
+		#f
+		(if (= (car lst) x)
+			#t
+			(helper (cdr lst) x))
+		)
+)
+
 (define (no-repeats lst) 
-		
+	(begin
+		(define (f res ls)
+			(if (null? ls)
+				res
+				(if (null? res)
+					(f (list (car ls)) (cdr lst))
+					(if (helper res (car ls))
+						(f res (cdr ls))
+						(f (append res (list (car ls))) (cdr ls))
+					)
+				)
+			)
+		)
+		(f nil lst)
+	)
 )
